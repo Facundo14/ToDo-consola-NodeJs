0fee9758c86a880ececa5f30ff1dd1ff96f7fcc3
Facundo14 <facundotanovich@gmail.com>
2022-02-14 16:35:46 -0300

    Fin Seccion 5 - App ToDo en consola NodeJs

---
+++ app.js
@@ -0,0 +1,65 @@
+require('colors');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+console.clear();
+
+
+const main = async() => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+    
+    if (tareasDB){
+        //Establecer las tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+    
+    do {
+        // Imprimir el manu
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                //crear opcion
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                tareas.listadoCompleto();
+            break;
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+            break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+            break;
+            case '5':
+                const ids = await mostrarListadoChecklist(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+            break;
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0'){
+                    const ok = await confirmar('¿Estás seguro?')
+                    if(ok){
+                        tareas.borrarTarea(id);
+                        console.log('Tarea Borrada');
+                    } 
+                }
+            break;
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        await pausa();
+
+    } while (opt !== '0');
+
+}
+
+main();
